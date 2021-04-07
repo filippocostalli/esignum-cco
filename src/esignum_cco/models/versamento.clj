e325de29a3a44d7a6718a0b102a3924eac0e5c49
fppo.costalli <filppocostalligmail.com>
2021-04-07 17:32:34 +0200

    Beginning to create pdf report

--- src/esignum_cco/models/versamento.clj
+++ src/esignum_cco/models/versamento.clj
@@ -2,7 +2,8 @@
   (:require
     [clojure.java.jdbc :as jdbc]
     [esignum-cco.utils :as utils]
-    [java-time :as time]))
+    [java-time :as time]
+    [clj-pdf.core :as pdf-core]))
 
 (def in-corso 0)
 (def terminato-ok 1)
@@ -15,3 +16,22 @@
                                                         :versamento_stato in-corso
                                                         :versamento_inizio (time/local-date)}
                     {:row-fn #(utils/format-output-keywords %)}))))
+
+
+(defn create-report [versamento]
+  (pdf-core/pdf
+    [{:header "ESIGNUM - Sistema di conservazione CCO - Marno S.r.l."}
+     [:image "resources/logo-marno.png"]
+     [:paragraph {:style :bold :size 16 :family :helvetica :color [61 92 92] :align :center}  "RAPPORTO DI VERSAMENTO"]
+     [:paragraph {:style :bold}  "DATI VERSAMENTO"]
+     [:table {:header [{:backdrop-color [100 100 100]} "ELEMENTO" "DESCRIZIONE"]}
+        [[:chunk "IDENTIFICATIVO VERSAMENTO"] [:phrase "baz"]]
+        [[:phrase "INDICE DI VERSAMENTO"] [:phrase "idv.xml aloa bellissimo"]]
+        [[:phrase "HASH IDV"] [:phrase "foo"]]]
+     [:paragraph {:style :bold} "\n\nDATI CONSERVAZIONE"]]
+    "e:/doc.pdf"))
+
+
+;;    INIZIO VERSAMENTO 01/02/2021 05:08:31
+;;    FINE VERSAMENTO 01/02/2021 09:43:34
+;;    ESITO VERSAMENTO))
