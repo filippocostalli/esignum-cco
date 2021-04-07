5f1e0570d37301b5340303d22c1842d69e914dfe
fppo.costalli <filppocostalligmail.com>
2021-04-07 14:15:52 +0200

    Handles destination directory

--- src/esignum_cco/services/conservazione.clj
+++ src/esignum_cco/services/conservazione.clj
@@ -6,14 +6,35 @@
     [esignum-cco.utils :as myutils]
     [esignum-cco.models.campionamento :as campionamento]
     [esignum-cco.models.versamento :as versamento]
-    [esignum-cco.services.unisincro :as unisincro]))
+    [esignum-cco.services.unisincro :as unisincro]
+    [java-time :as time]
+    [clojure.java.io :as io]))
+
+
+(defn get-dest-dir [campionamento-id versamento-id]
+  (let [root  config/pindex-file-root
+        now   (time/local-date)
+        year  (time/value (time/property now :year))
+        month (time/value (time/property now :month-of-year))
+        dest-dir (str root "/" year "/" month "/" campionamento-id "/" versamento-id)
+        _ (io/make-parents (str dest-dir "/."))]
+      (log/info (str "---- >" dest-dir))
+      dest-dir))
 
 
 (defn conservazione-campionamento! []
   (if-let [campionamento (campionamento/get-first config/db-regis)]
-    (let [new-versamento (versamento/insert! config/db-esignum campionamento)]
+     (let [versamento (versamento/insert! config/db-esignum campionamento)
+           _ (println versamento)
+           dest-dir (get-dest-dir (:sesscamp-id campionamento) (:versamento-id versamento))
+           campionamento-data (assoc campionamento :versamento versamento
+                                                   :destination-dir dest-dir)]
       (try
-          (-> campionamento
-              (unisincro/scatola->idc)) ;; tutti idc cartelle e idc one creati efottutamente salvati dova cazzo debbono
-              ;; inserisco il path nel versamento
-        (catch Exception e (println "Cagata:\n" e)))))) ;; qui deve segnare nel db che tutto è andato una merda
+          (-> campionamento-data
+              (assoc :idc-path (unisincro/scatola->idc campionamento-data)))
+              ;; inserisci in conservazione
+              ;; firma UnitaDiArchiviazioneCartelleCliniche
+              ;; crea rdv, update versamento,
+        (catch Exception e
+            ;; qui deve segnare nel db che tutto è andato una merda
+            (log/error {:module "user-feedback"} e "Versamento fallito"))))))
