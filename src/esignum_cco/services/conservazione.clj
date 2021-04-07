00615e4cb753d667c28ce0031f0b8531144c1b02
fppo.costalli <filppocostalligmail.com>
2021-04-07 10:14:23 +0200

    Start reading first campionamento available,  create versamento and create xml index for each cco

--- src/esignum_cco/services/conservazione.clj
+++ src/esignum_cco/services/conservazione.clj
@@ -1,22 +1,19 @@
-(ns esignum-cco.services.database
+(ns esignum-cco.services.conservazione
   (:require
     [clojure.string :as s]
     [cambium.core :as log]
     [esignum-cco.config :as config]
     [esignum-cco.utils :as myutils]
     [esignum-cco.models.campionamento :as campionamento]
+    [esignum-cco.models.versamento :as versamento]
     [esignum-cco.services.unisincro :as unisincro]))
 
-(def db-regis (:db-regis-spec config/configuration))
 
-(def not-nil? (complement nil?))
-
-(defn conservazione-campionamento!
-  (let [campionamento (campionamento/get-first db-regis)]
-    (try
-      (if (not-nil? campionamento)
-        (-> campionamento
-            (unisincro/scatola->idc))) ;; tutti idc cartelle e idc one creati efottutamente salvati dova cazzo debbono
-            ;; inserisco il path nel versamento del cazzo
-            ;; ritorna filepath udc -> inserire
-      (catch Exception e (println "Cagata:\n" e))))) ;; qui deve segnare nel db che tutto è andato una merda
+(defn conservazione-campionamento! []
+  (if-let [campionamento (campionamento/get-first config/db-regis)]
+    (let [new-versamento (versamento/insert! config/db-esignum campionamento)]
+      (try
+          (-> campionamento
+              (unisincro/scatola->idc)) ;; tutti idc cartelle e idc one creati efottutamente salvati dova cazzo debbono
+              ;; inserisco il path nel versamento
+        (catch Exception e (println "Cagata:\n" e)))))) ;; qui deve segnare nel db che tutto è andato una merda
